d423871d95d90d75f3a38e07441b2fa44654c509
zh12221 <114904813+zh12221@users.noreply.github.com>
2022-10-03 16:33:47 +0800

    Update index.cjs

--- 配置/index.cjs
+++ 配置/index.cjs
@@ -42,7 +42,8 @@ const USER_CONFIG = {
       // 我们在一起已经有xxxx天了的配置
       customizedDateList: [
         // 在一起的日子
-        { keyword: 'love_day', date: '2022-01-24' },      
+        { keyword: 'love_day', date: '2022-01-24' },
+        
       ],
     },
   ],
